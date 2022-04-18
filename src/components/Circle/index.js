844cc355ed15f0c95abf0c556e737b2eaad46568
bao hoa <hoa.nguyen4@monstar-lab.com>
2022-04-18 16:07:21 +0700

    lesson 4

---
+++ src/components/Circle/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Circle extends Component {
+  render() {
+    return (
+      <div>
+        <div className="circle">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Circle;

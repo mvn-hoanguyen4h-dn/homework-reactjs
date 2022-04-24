7a0012027331c760509ba2c005e3de314d0cca82
bao hoa <hoa.nguyen4@monstar-lab.com>
2022-04-25 00:16:20 +0700

    lesson-6 homework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Form from './components/Form/Table';
+import Form from './components/Form';
 import "./App.css"
 
 export default class App extends Component {
--- src/components/Form/Item.js
+++ src/components/Form/Item.js
@@ -5,29 +5,23 @@ export default class Item extends Component {
   handleLog(index) {
     const value = index
 
-    if (window.confirm("Are you sure to delete this item ?")) {
-      this.props.handleDelete(value)
-    }
+    this.props.handleDelete(value)
   }
 
   render() {
     const { fullname, email, password, nationality, gender, info, id } = this.props
     return (
-      <>
-        <tbody>
-          <tr>
-            <td>{fullname}</td>
-            <td>{email}</td>
-            <td>{password}</td>
-            <td>{nationality}</td>
-            <td>{gender}</td>
-            <td>{info}</td>
-            <td>
-              <i class="fa-solid fa-trash" onClick={() => this.handleLog(id)} ></i>
-            </td>
-          </tr>
-        </tbody>
-      </>
+      <tr>
+        <td>{fullname}</td>
+        <td>{email}</td>
+        <td>{password}</td>
+        <td>{nationality}</td>
+        <td>{gender}</td>
+        <td>{info}</td>
+        <td>
+          <i class="fa-solid fa-trash" onClick={() => this.handleLog(id)} ></i>
+        </td>
+      </tr>
     )
   }
 }
